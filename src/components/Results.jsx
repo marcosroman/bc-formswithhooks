8a83ffb49d4b39dbc6be68aa458d9670c50f7e05
marcosroman <marcosdromand@gmail.com>
2023-01-10 19:30:07 -0300

    bonificacion

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -8,12 +8,12 @@ const Results = props => {
 					<h3>Your form data</h3>
 					<p>First Name: {firstName}</p>
 					<p>Last Name: {lastName}</p>
-					{ (firstName.length < 2 || lastName.length < 2) &&
+					{ (firstName.length>0) && (firstName.length < 2 || lastName.length < 2) &&
 						(<p style={{'font-weight':'bold'}}>Nombre y apellido deben tener al menos 2 caracteres</p>) }
 					<p>Email: {email}</p>
-					{ email.length < 5 &&
+					{ email.length > 0 && email.length < 5 &&
 						(<p style={{'font-weight':'bold'}}>email debe tener al menos 5 caracteres</p>) }
-					{ password !== confirmPassword &&
+					{ (password.length>0 && confirmPassword.length>0 ) && password !== confirmPassword &&
 						(<p style={{'font-weight':'bold'}}>los passwords deben ser iguales!</p>) }
 				</div>
 		);
